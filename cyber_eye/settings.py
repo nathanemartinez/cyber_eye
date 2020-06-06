e2e07188d4a9c40f9c7b69e2b1fb245bd7dbc68f
Nathan Martinez <nathanemtz@gmail.com>
2020-06-05 18:45:04 -0700

    ...

--- cyber_eye/settings.py
+++ cyber_eye/settings.py
@@ -7,7 +7,7 @@ DATABASE_DIR = os.path.join(BASE_DIR, 'db.sqlite3')
 
 SECRET_KEY = os.environ.get('cyber_eye_secret_key')
 
-DEBUG = False
+DEBUG = True
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 # if DEBUG:
